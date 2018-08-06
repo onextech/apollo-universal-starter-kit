17932840fe589e73240bf24dfa2f3d1e0bfefc8a
clodal <joel.koh@me.com>
2018-08-06 19:05:35 +0800

    minor update gplayground

--- packages/server/src/middleware/gplayground.js
+++ packages/server/src/middleware/gplayground.js
@@ -16,7 +16,7 @@ export default (req) => {
     tabs: [
       {
         endpoint: '/graphql',
-        query: '{\n' + '  serverCounter {\n' + '    amount\n' + '  }\n' + '}',
+        query: '{\n' + '  currentUser {\n' + '    id\n' + '  }\n' + '}',
       },
     ],
   })
